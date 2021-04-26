53891ed0909c2660afbfe1d2dd744d5aea3215a9
AMAN ANKU <amankumaranku@gmail.com>
2021-04-26 07:54:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styles from "./App.module.css"
+import List from "./components/List"
 
-function App() {
+class App extends React.Component {
+ render(){ 
+   const name = "navgurukul"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <List props={name} />
+    <p className={styles.aman}> aman </p> 
+    <h1 className={styles.hello}>Hello</h1>   
     </div>
   );
 }
-
+}
 export default App;
+
+// class components
---
+++ src/components/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function List (props) {
+        return(
+            <div>
+             <h1>List</h1>
+             <h5>{props.props}</h5>
+            </div>
+        )
+    }
+export default List
